d895f9cd755ccf687bcd0db7447bf364d8358f51
vanshika1507 <122568086+vanshika1507@users.noreply.github.com>
2024-10-09 23:50:06 +0530

    Create main.js

---
+++ catalog/static/js/main.js
@@ -0,0 +1,27 @@
+const element = document.getElementById('content');
+const modal = document.getElementById('productModal');
+const menu = document.getElementById("menu");
+function showDropdown() {
+    element.classList.toggle('show');
+}
+
+function showModal(x) {
+    const productName = document.getElementById('productName');
+    const productImage = document.getElementById("productImage");
+
+    productName.innerText = x.name;
+
+    productImage.setAttribute("src", x.image);
+
+    modal.classList.toggle('show')
+
+}
+
+function closeModal() {
+    modal.classList.add('show')
+}
+function showMenu() {
+
+    menu.classList.toggle("hidden");
+
+}
